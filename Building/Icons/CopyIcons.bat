b375cbbaaddf0dcf0848848becc1155d3ed99fcf
Seeker4 <guy.oscar@moxis.com>
2019-05-11 23:29:19 -0700

    New set of icons from Ruth, with outer glow effect.  Put work files in new Building/Icons directory.

---
+++ Building/Icons/CopyIcons.bat
@@ -0,0 +1,11 @@
+@echo off
+setlocal
+set destdir=..\..\oscar\icons
+
+copy logo.ico %destdir%\logo.ico
+copy oscar.icns %destdir%\oscar.icns
+
+copy full-220.png %destdir%\logo-lg.png
+copy full-100.png %destdir%\logo-lm.png
+copy full-64.png %destdir%\logo-md.png
+copy wave-24.png %destdir%\logo-sm.png
