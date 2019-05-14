3e5d3a901bca49e576f6b2526a23e9b00fcb136a
Seeker4 <guy.oscar@moxis.com>
2019-05-13 21:47:00 -0700

    Set window icon to 32x32 rather than 24x24.

--- Building/Icons/CopyIcons.bat
+++ Building/Icons/CopyIcons.bat
@@ -8,4 +8,4 @@ copy oscar.icns %destdir%\oscar.icns
 copy full-220.png %destdir%\logo-lg.png
 copy full-100.png %destdir%\logo-lm.png
 copy full-64.png %destdir%\logo-md.png
-copy wave-24.png %destdir%\logo-sm.png
+copy wave-32.png %destdir%\logo-sm.png
